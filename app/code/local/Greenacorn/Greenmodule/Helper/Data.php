f2d5e453f9cfcbe5d267d8da7b5787c4b8975bd7
Sean Walther <seangw@seangw.com>
2014-12-02 17:27:08 -0500

    Code migration

---
+++ app/code/local/Greenacorn/Greenmodule/Helper/Data.php
@@ -0,0 +1,4 @@
+<?php 
+class Greenacorn_Greenmodule_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
---
+++ app/code/local/SproutingThreads/STAdminModule/Helper/Data.php
@@ -0,0 +1,4 @@
+<?php
+class SproutingThreads_STAdminModule_Data extends Mage_Core_Helper_Abstract
+{
+} 
